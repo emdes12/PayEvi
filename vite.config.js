dac62885cfa1bc3ddc343d910243a586213d10e7
Alawiye Muritala <naijabayz@gmail.com>
2023-02-09 11:00:25 +0100

    reclone

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,22 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+});
